94ca646eae94a187ce8ec57237d54d60a34b852d
Ansifali <alinansif369@gmail.com>
2024-03-18 11:12:00 +0530

    fix bug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const Swal = require('sweetalert2');//////atert messasge
 const session = require('express-session');
-const mongoose = require("../project/server")
+// const mongoose = require("../project/server")
 const app = express();
 const port = 3001;
 require('dotenv').config();
