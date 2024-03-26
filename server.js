775f17c68a3760343279ab57a1dd8f8cf1817dae
Ansifali <alinansif369@gmail.com>
2024-03-26 19:53:42 +0530

    fix the bug

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 // Mongoose connection
 mongoose.connect(process.env.MONGOSTR)
   .then(() => {
-    console.log("MongoDB is connected");
+    console.log("MongoDB is connected successfully");
   })
   .catch((err) => {
     console.log('MongoDB connection error:', err);
